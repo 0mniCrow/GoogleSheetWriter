0dadbae167f112131243a3cd31c01adcf8c7222a
OmniCrow <4qui10nis@gmail.com>
2025-05-29 23:48:56 +0300

    continuing changing save file structure.

--- xmlparser.cpp
+++ xmlparser.cpp
@@ -4,3 +4,11 @@ XMLParser::XMLParser()
 {
 
 }
+bool XMLParser::saveData(const QString& addr, const QMap<QString,QString>& data)
+{
+    return true;
+}
+bool XMLParser::loadData(const QString& addr, QMap<QString,QString>& container)
+{
+    return true;
+}
