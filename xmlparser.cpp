50bdc80ddc498f3a9278f5e6e0d110423622f2c1
OmniCrow <4qui10nis@gmail.com>
2025-05-31 23:40:47 +0300

    Continuing adding file manager with xml.

--- xmlparser.cpp
+++ xmlparser.cpp
@@ -8,6 +8,11 @@ bool XMLParser::saveData(const QString& addr, const QMap<QString,QString>& data)
 {
     QDomDocument saveFile("savefile");
     QFile file(addr);
+    if(!file.open(QIODevice::WriteOnly))
+    {
+        return false;
+    }
+
     /**/
     return true;
 }
@@ -15,5 +20,17 @@ bool XMLParser::loadData(const QString& addr, QMap<QString,QString>& container)
 {
     QDomDocument saveFile("savefile");
     QFile file(addr);
+    if(!file.open(QIODevice::ReadOnly))
+    {
+        return false;
+    }
+    if(!saveFile.setContent(&file))
+    {
+        file.close();
+        return false;
+    }
+    file.close();
+
+    QDomElement docElem(saveFile.documentElement());
     return true;
 }
