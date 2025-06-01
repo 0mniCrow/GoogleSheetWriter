63509c36a9e09decd2a492c48f39e4cfca5446a4
OmniCrow <4qui10nis@gmail.com>
2025-06-01 22:12:17 +0300

    Continuing adding xml file setting save.

--- xmlparser.cpp
+++ xmlparser.cpp
@@ -6,31 +6,42 @@ XMLParser::XMLParser()
 }
 bool XMLParser::saveData(const QString& addr, const QMap<QString,QString>& data)
 {
-    QDomDocument saveFile("savefile");
+    QDomDocument Settings("savefile");
+    QDomElement koranj(Settings.createElement("Settings"));
+    Settings.appendChild(koranj);
+
     QFile file(addr);
     if(!file.open(QIODevice::WriteOnly))
     {
         return false;
     }
 
+
     /**/
     return true;
 }
 bool XMLParser::loadData(const QString& addr, QMap<QString,QString>& container)
 {
-    QDomDocument saveFile("savefile");
+    QDomDocument Settings("savefile");
     QFile file(addr);
     if(!file.open(QIODevice::ReadOnly))
     {
         return false;
     }
-    if(!saveFile.setContent(&file))
+    if(!Settings.setContent(&file))
     {
         file.close();
         return false;
     }
     file.close();
-
-    QDomElement docElem(saveFile.documentElement());
+    container.clear();
+    QDomElement docElem(Settings.documentElement());
+    QDomNode n = docElem.firstChild();
+    while(!n.isNull())
+    {
+        QDomElement e = n.toElement();
+        container.insert(e.tagName(),e.text());
+        n = n.nextSibling();
+    }
     return true;
 }
